af4572f2a0118e309986864f2d1349026ace1ce5
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-15 00:24:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,5 +7,6 @@ def key_for_min_value(name_hash)
   
   name_hash.each do |key, value|
     if value < min_value || min_value == 0 
-      min_value = 
+      min_value = value 
+      min_key = key 
 end
